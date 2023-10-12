27d17de7d02959cacd83b3d4fb987296dfc2c8f5
Lev Shapovalov <Leff200045@Gmail.com>
2023-10-12 09:50:02 +0500

    Added model Task

---
+++ src/main/java/org/example/taskService/model/Task.java
@@ -0,0 +1,30 @@
+package org.example.taskService.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is mandatory")
+    private String title;
+
+    @NotBlank(message = "Description is mandatory")
+    private String description;
+
+    private LocalDate date;
+
+    private boolean completed;
+}
